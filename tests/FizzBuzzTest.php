58e675e53c8ef9b18fae9873ec90f1a2818dd27d
Joe Fearnley <joe.fearnley@gmail.com>
2016-01-16 23:40:24 -0500

    Doing a little refactoring.

--- tests/FizzBuzzTest.php
+++ tests/FizzBuzzTest.php
@@ -9,42 +9,42 @@ class FizzBuzzTest extends PHPUnit_Framework_TestCase
         $this->fizzBuzz = new FizzBuzz();
     }
 
-    public function test1Translates1ForFizzbuzz()
+    public function test_1_translates_1_for_fizzbuzz()
     {
         $this->assertEquals(1, $this->fizzBuzz->execute(1));
     }
 
-    public function test2Translates2ForFizzbuzz()
+    public function test_2__translates_2_for_fizzbuzz()
     {
         $this->assertEquals(2, $this->fizzBuzz->execute(2));
     }
 
-    public function test3TranslatesFizzForFizzbuzz()
+    public function test_3_translates_fizz_for_fizzbuzz()
     {
         $this->assertEquals('fizz', $this->fizzBuzz->execute(3));
     }
 
-    public function test99TranslatesFizzForFizzbuzz()
+    public function test_99_translates_fizz_for_fizzbuzz()
     {
         $this->assertEquals('fizz', $this->fizzBuzz->execute(99));
     }
 
-    public function test5TranslatesBuzzForFizzbuzz()
+    public function test_5_translates_buzz_for_fizzbuzz()
     {
         $this->assertEquals('buzz', $this->fizzBuzz->execute(5));
     }
 
-    public function test10TranslatesBuzzForFizzbuzz()
+    public function test_10_translates_buzz_for_fizzbuzz()
     {
         $this->assertEquals('buzz', $this->fizzBuzz->execute(10));
     }
 
-    public function test15TranslatesFizzBuzzForFizzbuzz()
+    public function test_15_translates_fizzbuzz_for_fizzbuzz()
     {
         $this->assertEquals('fizzbuzz', $this->fizzBuzz->execute(15));
     }
 
-    public function testItTranslatesASequenceIfNumbersForFizzbuzz()
+    public function test_it_translates_a_sequence_of_numbers_for_fizzbuzz()
     {
         $this->assertEquals([1, 2, 'fizz', 4, 'buzz'], $this->fizzBuzz->executeUpTo(5));
     }
